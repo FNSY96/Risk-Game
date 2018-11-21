1e172d92564524966ca62e9814c91ed2c833b2f8
karemgerges <kimogerges55@gmail.com>
2018-11-21 21:37:28 +0200

    add the main functions of the game
    attack function
    deploy troops
    increase player vertices

--- src/gameModeling/Player.java
+++ src/gameModeling/Player.java
@@ -2,10 +2,10 @@ package gameModeling;
 
 public class Player {
     private int availableTroops;
-//    private boolean ownsContinent;
-
+    private int numberOfOwnedVertices;
     public Player() {
-//        this.ownsContinent = false;
+        this.availableTroops = 0;
+        this.numberOfOwnedVertices = 0;
     }
 
     public void setAvailableTroops(int troops) {
@@ -22,6 +22,31 @@ public class Player {
 
     public void removeTroops() {
         this.availableTroops = 0;
+    }
+    
+    public int getNumberOfOwnedVertices()
+    {
+    	return this.numberOfOwnedVertices;
+    }
+    
+    public void incrementNumberOfOwnedVertices()
+    {
+    	this.numberOfOwnedVertices++;
+    }
+    
+    public void decrementNumberOfOwnedVertices()
+    {
+    	 this.numberOfOwnedVertices-- ;
+    }
+    
+    public void setNumberOfOwnedVertices(int numberOfOwnedVertices)
+    {
+    	this.numberOfOwnedVertices = numberOfOwnedVertices;
+    }
+    
+    public boolean doesNotOwnVertices()
+    {
+    	return (this.numberOfOwnedVertices == 0);
     }
 
 //    public void setOwnsContinent(boolean ownsContinent) {
