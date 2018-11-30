1416f12f1e52eefd77f109a546f2818fd111f442
FNSY <fadynabil96@gmail.com>
2018-11-30 15:45:55 +0200

    GUI is imported with minor bugs!

---
+++ src/gui/GUIGame.java
@@ -0,0 +1,299 @@
+package gui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.ui.swingViewer.ViewPanel;
+import org.graphstream.ui.view.Viewer;
+
+import gameDriver.GameDriver;
+import gameDriver.PlayersTypes;
+
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import java.awt.Font;
+
+public class GUIGame extends JFrame {
+    static GUIGame frame;
+    JPanel panel;
+    private JPanel contentPane;
+    private JTextField troopsField;
+    private JTextField toattField;
+    private JTextField fromattField;
+    private GameDriver d;
+    Viewer viewer;
+    Graph graph;
+    int[] playerType;
+    JLabel lblPlayerNo;
+    JLabel lbltroops;
+    JButton btnDrop;
+    JButton btnAttack;
+    JButton btnFinishTurn;
+    private JLabel lblPlayer;
+    private JLabel lblPlayer_1;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    frame = new GUIGame(2, 2);
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the frame.
+     */
+    public GUIGame(int p1, int p2) {
+
+        initializeGameDriver(p1, p2);
+        playerType = new int[2];
+        playerType[0] = p1;
+        playerType[1] = p2;
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(0, 0, 1366, 750);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+
+        troopsField = new JTextField();
+        troopsField.setBounds(950, 138, 86, 20);
+        contentPane.add(troopsField);
+        troopsField.setColumns(10);
+
+        JLabel lblDropTroopsAt = new JLabel("drop troops at");
+        lblDropTroopsAt.setBounds(950, 88, 86, 39);
+        contentPane.add(lblDropTroopsAt);
+
+        lbltroops = new JLabel("28");
+        lbltroops.setBounds(1040, 141, 46, 14);
+        contentPane.add(lbltroops);
+
+        btnDrop = new JButton("drop");
+        btnDrop.setBounds(947, 179, 89, 23);
+        contentPane.add(btnDrop);
+        btnDrop.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                int drop = Integer.parseInt(troopsField.getText());
+                if (d.getGame().getGraph().getOwner(drop) == d.getTurn()) {
+                    d.playHumanDeploymentTurn(drop);
+                    troopsField.setText("");
+                    updateGraph();
+                } else {
+
+                    int pane = JOptionPane.showConfirmDialog(frame, "error you don't own :" + drop, "ERROR",
+                            JOptionPane.DEFAULT_OPTION);
+
+                }
+            }
+        });
+
+        lblPlayerNo = new JLabel("player no: ");
+        lblPlayerNo.setBounds(950, 11, 86, 30);
+        contentPane.add(lblPlayerNo);
+
+        JLabel lblAttackFrom = new JLabel("attack from :");
+        lblAttackFrom.setBounds(950, 237, 86, 20);
+        contentPane.add(lblAttackFrom);
+
+        fromattField = new JTextField();
+        fromattField.setBounds(950, 268, 86, 20);
+        contentPane.add(fromattField);
+        fromattField.setColumns(10);
+
+        JLabel lblTo = new JLabel("to :");
+        lblTo.setBounds(950, 311, 86, 23);
+        contentPane.add(lblTo);
+
+        toattField = new JTextField();
+        toattField.setBounds(950, 354, 86, 20);
+        contentPane.add(toattField);
+        toattField.setColumns(10);
+
+        btnAttack = new JButton("attack");
+        btnAttack.setBounds(950, 413, 89, 23);
+        contentPane.add(btnAttack);
+        btnAttack.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                int fromatt = Integer.parseInt(fromattField.getText());
+                int toatt = Integer.parseInt(toattField.getText());
+                if (d.getGame().canAttack(fromatt, toatt)) {
+                    d.playHumanAttackTurn(fromatt, toatt);
+                    fromattField.setText("");
+                    toattField.setText("");
+                    updateGraph();
+                } else {
+
+                    int pane = JOptionPane.showConfirmDialog(frame, "Illegal attack from " + fromatt + " to " + toatt,
+                            "ERROR", JOptionPane.DEFAULT_OPTION);
+
+                }
+            }
+        });
+
+        btnFinishTurn = new JButton("finish turn");
+        btnFinishTurn.setBounds(950, 498, 108, 23);
+        contentPane.add(btnFinishTurn);
+
+        lblPlayer = new JLabel("player 1");
+        lblPlayer.setFont(new Font("Tw Cen MT Condensed Extra Bold", Font.ITALIC, 20));
+        lblPlayer.setForeground(new Color(0, 51, 255));
+        lblPlayer.setBackground(new Color(255, 255, 255));
+        lblPlayer.setBounds(950, 569, 131, 46);
+        contentPane.add(lblPlayer);
+
+        lblPlayer_1 = new JLabel("player 2");
+        lblPlayer_1.setFont(new Font("Tw Cen MT Condensed Extra Bold", Font.ITALIC, 20));
+        lblPlayer_1.setBackground(new Color(255, 255, 255));
+        lblPlayer_1.setForeground(new Color(255, 102, 0));
+        lblPlayer_1.setBounds(950, 615, 124, 54);
+        contentPane.add(lblPlayer_1);
+        btnFinishTurn.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                if (playerType[d.getTurn()] > 3) {
+                    d.playAITurn();
+                } else if (playerType[d.getTurn()] != 2) {
+                    d.playDeploymentTurn();
+                    d.playAttackTurn();
+                } else {
+                    // human finishes turn without attacking
+                }
+                updateGraph();
+            }
+        });
+
+        drawGraph();
+
+    }
+
+    void updateControlState() {
+        panel.setBorder(BorderFactory.createLineBorder(Color.blue, 5));
+        if (d.getTurn() == 1) {
+            panel.setBorder(BorderFactory.createLineBorder(Color.orange, 5));
+        }
+        if (d.getGame().gameEnded()) {
+            int pane = JOptionPane.showConfirmDialog(frame, "player : " + (d.getGame().getGraph().getOwner(1) + 1) + " won the Game", "Game is finished", JOptionPane.DEFAULT_OPTION);
+            frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+        }
+        lblPlayerNo.setText("player no:" + (d.getTurn() + 1));
+        lbltroops.setText(d.getGame().getPlayer(d.getTurn()).getAvailableTroops() + "");
+        troopsField.setEnabled(true);
+        btnDrop.setEnabled(true);
+        fromattField.setEnabled(true);
+        toattField.setEnabled(true);
+        btnAttack.setEnabled(true);
+        if (playerType[d.getTurn()] != 2) {
+            troopsField.setEnabled(false);
+            btnDrop.setEnabled(false);
+            fromattField.setEnabled(false);
+            toattField.setEnabled(false);
+            btnAttack.setEnabled(false);
+        }
+    }
+
+    void drawGraph() {
+
+        ArrayList<Integer>[] adjacencyList = d.getGame().getGraph().adjacencyList;
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel = new JPanel(new GridLayout()) {
+            @Override
+            public Dimension getPreferredSize() {
+                return new Dimension(830, 600);
+            }
+        };
+        // panel.setSize(800, 600);
+        panel.setLocation(120, 120);
+
+
+        graph = new SingleGraph("Risk", false, true);
+
+        // System.setProperty("org.graphstream.ui.renderer",
+        // "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
+        for (Integer i = 1; i < adjacencyList.length; i++) {
+
+            graph.addNode(i.toString()).addAttribute("ui.label",
+                    "(" + i + ")  Troops:" + d.getGame().getGraph().getTroopsInVertex(i) + "  C:"
+                            + d.getGame().getGraph().getContinentOfVertex(i));
+            if (d.getGame().getGraph().getOwner(i) == 0) {
+                graph.getNode(i.toString()).addAttribute("ui.style", "size: 50px; fill-color: rgb(0,100,255);");
+            } else {
+                graph.getNode(i.toString()).addAttribute("ui.style", "size: 50px; fill-color: rgb(255,100,0);");
+            }
+        }
+        for (Integer i = 1; i < adjacencyList.length; i++) {
+            for (Integer j = 0; j < adjacencyList[i].size(); j++) {
+
+                graph.addEdge(i + "+" + adjacencyList[i].get(j), i.toString(), adjacencyList[i].get(j).toString());
+            }
+        }
+        viewer = new Viewer(graph, Viewer.ThreadingModel.GRAPH_IN_GUI_THREAD);
+        viewer.enableAutoLayout();
+        ViewPanel viewPanel = viewer.addDefaultView(false);
+        panel.add(viewPanel);
+        frame.getContentPane().add(panel);
+        frame.pack();
+        // frame.setLocationRelativeTo(null);
+        // frame.setVisible(true);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        contentPane.add(panel);
+        updateControlState();
+
+    }
+
+    void updateGraph() {
+
+        ArrayList<Integer>[] adjacencyList = d.getGame().getGraph().adjacencyList;
+        for (Integer i = 1; i < adjacencyList.length; i++) {
+            graph.getNode(i.toString()).addAttribute("ui.label",
+                    "(" + i + ")  Troops:" + d.getGame().getGraph().getTroopsInVertex(i) + "  C:"
+                            + d.getGame().getGraph().getContinentOfVertex(i));
+            if (d.getGame().getGraph().getOwner(i) == 0) {
+                graph.getNode(i.toString()).addAttribute("ui.style", "size: 50px; fill-color: rgb(0,100,255);");
+            } else {
+                graph.getNode(i.toString()).addAttribute("ui.style", "size: 50px; fill-color: rgb(255,100,0);");
+            }
+        }
+        updateControlState();
+    }
+
+    void initializeGameDriver(int p1, int p2) {
+        d = new GameDriver(PlayersTypes.values()[p1], PlayersTypes.values()[p2]);
+
+    }
+}
---
+++ src/gui/Start.java
@@ -0,0 +1,255 @@
+package gui;
+
+import gameDriver.PlayersTypes;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+
+import javax.swing.JLabel;
+import java.awt.Color;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Start extends JFrame {
+
+    private JPanel contentPane;
+    private final ButtonGroup buttonGroup = new ButtonGroup();
+    private final ButtonGroup buttonGroup_1 = new ButtonGroup();
+    int p1, p2;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Start frame = new Start();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the frame.
+     */
+    public Start() {
+        setBackground(Color.RED);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 624, 587);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+
+        JRadioButton rdbtnNewRadioButton_3 = new JRadioButton(PlayersTypes.PACIFIST.toString());
+        buttonGroup.add(rdbtnNewRadioButton_3);
+        rdbtnNewRadioButton_3.setBounds(70, 277, 109, 23);
+        contentPane.add(rdbtnNewRadioButton_3);
+        rdbtnNewRadioButton_3.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                p1 = 3;
+            }
+        });
+
+        JRadioButton rdbtnNewRadioButton_4 = new JRadioButton(PlayersTypes.GREEDY.toString());
+        buttonGroup.add(rdbtnNewRadioButton_4);
+        rdbtnNewRadioButton_4.setBounds(70, 333, 109, 23);
+        contentPane.add(rdbtnNewRadioButton_4);
+        rdbtnNewRadioButton_4.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                p1 = 4;
+            }
+        });
+
+        JRadioButton rdbtnNewRadioButton_5 = new JRadioButton(PlayersTypes.A_STAR.toString());
+        buttonGroup.add(rdbtnNewRadioButton_5);
+        rdbtnNewRadioButton_5.setBounds(70, 387, 109, 23);
+        contentPane.add(rdbtnNewRadioButton_5);
+        rdbtnNewRadioButton_5.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                p1 = 5;
+            }
+        });
+
+
+        JRadioButton rdbtnNewRadioButton_6 = new JRadioButton(PlayersTypes.REAL_TIME_A_STAR.toString());
+        buttonGroup.add(rdbtnNewRadioButton_6);
+        rdbtnNewRadioButton_6.setBounds(70, 430, 109, 23);
+        contentPane.add(rdbtnNewRadioButton_6);
+        rdbtnNewRadioButton_6.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                p1 = 6;
+            }
+        });
+
+        JRadioButton radioButton = new JRadioButton(PlayersTypes.AGGRESSIVE.toString());
+        radioButton.setSelected(true);
+        buttonGroup_1.add(radioButton);
+        radioButton.setBounds(373, 118, 109, 23);
+        contentPane.add(radioButton);
+        radioButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                p2 = 0;
+            }
+        });
+
+        JRadioButton radioButton_1 = new JRadioButton(PlayersTypes.PASSIVE.toString());
+        buttonGroup_1.add(radioButton_1);
+        radioButton_1.setBounds(373, 166, 109, 23);
+        contentPane.add(radioButton_1);
+        radioButton_1.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                p2 = 1;
+            }
+        });
+
+        JRadioButton radioButton_2 = new JRadioButton(PlayersTypes.HUMAN.toString());
+        buttonGroup_1.add(radioButton_2);
+        radioButton_2.setBounds(373, 220, 109, 23);
+        contentPane.add(radioButton_2);
+        radioButton_2.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                p2 = 2;
+            }
+        });
+
+        JRadioButton radioButton_3 = new JRadioButton(PlayersTypes.PACIFIST.toString());
+        buttonGroup_1.add(radioButton_3);
+        radioButton_3.setBounds(373, 277, 109, 23);
+        contentPane.add(radioButton_3);
+        radioButton_3.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                p2 = 3;
+            }
+        });
+
+        JRadioButton radioButton_4 = new JRadioButton(PlayersTypes.GREEDY.toString());
+        buttonGroup_1.add(radioButton_4);
+        radioButton_4.setBounds(373, 333, 109, 23);
+        contentPane.add(radioButton_4);
+        radioButton_4.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                p2 = 4;
+            }
+        });
+
+        JRadioButton radioButton_5 = new JRadioButton(PlayersTypes.A_STAR.toString());
+        buttonGroup_1.add(radioButton_5);
+        radioButton_5.setBounds(373, 387, 109, 23);
+        contentPane.add(radioButton_5);
+        radioButton_5.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                p2 = 5;
+            }
+        });
+
+        JRadioButton radioButton_6 = new JRadioButton(PlayersTypes.REAL_TIME_A_STAR.toString());
+        buttonGroup_1.add(radioButton_6);
+        radioButton_6.setBounds(373, 430, 109, 23);
+        contentPane.add(radioButton_6);
+        radioButton_6.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                p2 = 6;
+            }
+        });
+
+        JLabel lblPlayer = new JLabel("player 1");
+        lblPlayer.setBounds(70, 24, 46, 14);
+        contentPane.add(lblPlayer);
+
+        JLabel lblPlayer_1 = new JLabel("player 2");
+        lblPlayer_1.setBounds(373, 24, 46, 14);
+        contentPane.add(lblPlayer_1);
+
+        JRadioButton radioButton_7 = new JRadioButton(PlayersTypes.AGGRESSIVE.toString());
+        buttonGroup.add(radioButton_7);
+        radioButton_7.setSelected(true);
+        radioButton_7.setBounds(70, 118, 109, 23);
+        contentPane.add(radioButton_7);
+        radioButton_7.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                p1 = 0;
+            }
+        });
+
+        JRadioButton radioButton_8 = new JRadioButton(PlayersTypes.PASSIVE.toString());
+        buttonGroup.add(radioButton_8);
+        radioButton_8.setBounds(70, 166, 109, 23);
+        contentPane.add(radioButton_8);
+        radioButton_8.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                p1 = 1;
+            }
+        });
+
+        JRadioButton radioButton_9 = new JRadioButton(PlayersTypes.HUMAN.toString());
+        buttonGroup.add(radioButton_9);
+        radioButton_9.setBounds(70, 220, 109, 23);
+        contentPane.add(radioButton_9);
+        radioButton_8.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                p1 = 2;
+            }
+        });
+
+        JButton btnOk = new JButton("OK");
+        btnOk.setBounds(210, 488, 89, 23);
+        contentPane.add(btnOk);
+        btnOk.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                EventQueue.invokeLater(new Runnable() {
+                    public void run() {
+                        try {
+                            dispose();
+                            GUIGame frame = new GUIGame(p1, p2);
+                            frame.setVisible(true);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
